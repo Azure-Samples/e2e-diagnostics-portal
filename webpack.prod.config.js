b32ad4a590be394265163882276eb55ff5708f7a
Zhiqing Qiu <zhqqi@microsoft.com>
2018-01-29 19:09:49 +0800

    Change deploy script

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = [
     entry: './src/index.js',
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'build/web')
+      path: path.resolve(__dirname, 'build')
     },
     module: {
       rules: [
